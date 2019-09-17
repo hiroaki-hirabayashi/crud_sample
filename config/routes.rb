1d07771e289400a33164b54664082cabc1e8ab24
平林 宏淳 <hh@MacBook-Pro.local>
2019-09-17 16:49:05 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#一覧画面
-  get '/users' => 'users#index'
-#新規投稿
-  get '/users/new' => 'users#new'
-#新規登録
-  post '/users' => 'users#create'
-#編集画面
-  get '/users/:id/edit' => 'users#edit'
-#更新
-patch '/users/:id' => 'users#update'
-#削除
-delete '/users/:id' => 'users#destroy'
-
+# #一覧画面
+#   get '/users' => 'users#index'
+# #新規投稿
+#   get '/users/new' => 'users#new'
+# #新規登録
+#   post '/users' => 'users#create'
+# #編集画面
+#   get '/users/:id/edit' => 'users#edit'
+# #更新
+# patch '/users/:id' => 'users#update'
+# #削除
+# delete '/users/:id' => 'users#destroy'
+resources :users
 end
